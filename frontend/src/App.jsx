4553113da5f884e5729d5f5d976c77522f8670ad
Bijoy <bijocode@gmail.com>
2026-05-18 18:44:22 +0530

    Routing created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
-import Navbar from "./components/Navbar"
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Body from "./components/Body"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 const App = () => {
   return (
-    <div className=""><Navbar/></div>
+    <div >
+
+      <BrowserRouter basename='/'>
+      <Routes>
+      <Route path='/' element={<Body/>}>
+        <Route path='login' element={<Login/>}/>
+        <Route path='profile' element={<Profile/>}/>
+      </Route>
+     
+      </Routes>
+      </BrowserRouter>
+     
+    </div>
   )
 }
 export default App
