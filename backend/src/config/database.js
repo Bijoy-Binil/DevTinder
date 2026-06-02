401b4703a8d336ab3bbbd33b281027367f13e500
Bijoy <bijocode@gmail.com>
2026-06-03 00:22:31 +0530

    v2

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const DB_URL=process.env.DB_URL
-// mongoose.connect("mongodb+srv://bijoy_db:Bijoy@mongodb@cluster0.l6kl9ia.mongodb.net/")
+
 
 const connectDB= async () => {
     try {
